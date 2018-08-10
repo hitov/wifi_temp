c5ac912704812d7902b8dc920b7660808af5b5d7
LZ1ARM <lz1arm@yahoo.com>
2018-08-11 00:34:38 +0300

    Fix getMAC address function

--- dweet-temp2.ino
+++ dweet-temp2.ino
@@ -37,7 +37,8 @@ String getMAC()
   uint8_t mac[6];
   char result[14];
 
- snprintf( result, sizeof( result ), "%02x%02x%02x%02x%02x%02x", mac[ 0 ], mac[ 1 ], mac[ 2 ], mac[ 3 ], mac[ 4 ], mac[ 5 ] );
+  WiFi.macAddress(mac);
+  snprintf( result, sizeof( result ), "%02x%02x%02x%02x%02x%02x", mac[ 0 ], mac[ 1 ], mac[ 2 ], mac[ 3 ], mac[ 4 ], mac[ 5 ] );
 
   return String( result );
 }
